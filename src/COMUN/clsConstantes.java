317d4b2524585c1702faf3c69668821366df6cc2
Unknown <yeray.lamelas@opendeusto.es>
2019-03-21 10:46:10 +0100

    Todo bien

--- src/COMUN/clsConstantes.java
+++ src/COMUN/clsConstantes.java
@@ -1,5 +1,45 @@
-package Comun;
+package COMUN;
 
 public class clsConstantes {
+
+	public enum enFicDatos {
+
+	}
+
+	public static final String clsFACTURA_IDENTIFICADOR = "identificador";
+	public static final String clsFACTURA_FECHA = "fecha";
+
+	public static final String clsEMPLEADO_IDENTIFICADOR = "identificador";
+	public static final String clsEMPLEADO_NOMBRE = "nombre";
+	public static final String clsEMPLEADO_APELLIDO = "apellido";
+	public static final String clsEMPLEADO_PUESTO = "puesto";
+
+	public static final String clsEMPRESA_IDENTIFICADOR = "identificador";
+	public static final String clsEMPRESA_NOMBRE = "nombre";
+	public static final String clsEMPRESA_CIUDAD = "ciudad";
+
+	public static final String clsCIUDAD_IDENTIFICADOR = "identificador";
+	public static final String clsCIUDAD_NOMBRE = "nombre";
+
+	public static final String clsCLIENTES_IDENTIFICADOR = "identificador";
+	public static final String clsCLIENTES_NOMBRE = "nombre";
+	public static final String clsCLIENTES_APELLIDO = "apellido";
+	public static final String clsCLIENTES_CORREO = "correo";
+
+	public static final String clsPERSONA_IDENTIFICADOR = "identificador";
+	public static final String clsPERSONA_NOMBRE = "nombre";
+	public static final String clsPERSONA_APELLIDO = "apellido";
+
+	public static final String clsPRODUCTO_IDENTIFICADOR = "identificador";
+	public static final String clsPRODUCTO_TIPO = "tipo";
+	public static final String clsPRODUCTO_MARCA = "marca";
+
+	public static final String clsPROVEEDOR_IDENTIFICADOR = "identificador";
+	public static final String clsPROVEEDOR_NOMBRE = "nombre";
+	public static final String clsPROVEEDOR_CIUDAD = "ciudad";
+
+	public static final String clsSUCURSAL_IDENTIFICADOR = "identificador";
+	public static final String clsSUCURSAL_NOMBRE = "nombre";
+	public static final String clsSUCURSAL_CIUDAD = "ciudad";
 
 }
